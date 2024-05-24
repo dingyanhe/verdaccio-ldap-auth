8fd2db62a3b78f33cb7b8b83d4bbd5409c02f173
丁言河D0362319 <dingyanhe@100.me>
2024-05-24 10:40:20 +0800

    chore: 处理构建异常问题

---
+++ rollup.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'rollup'
+import resolver from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default defineConfig({
+	input: 'src/index.ts',
+	output: {
+		file: 'dist/bundle.js',
+		format: 'cjs',
+		sourcemap: true,
+	},
+	plugins: [
+		resolver(),
+
+		json(),
+		commonjs(),
+		typescript({
+			tsconfig: './tsconfig.json',
+		}),
+		
+		isProduction && (await import('@rollup/plugin-terser')).default()
+	]
+})
