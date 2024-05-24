52a2cf9951767524eaca8c0bff5dff423889324e
丁言河D0362319 <dingyanhe@100.me>
2024-05-24 18:31:04 +0800

    chore: 处理报错

---
+++ rollup.config.ts
@@ -0,0 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
+
+import { defineConfig } from 'rollup'
+
+export default defineConfig({
+	input: 'src/index.ts',
+	output: {
+		file: 'dist/bundle.js',
+		format: 'cjs'
+	},
+	plugins: [resolve({ extensions: ['.ts'] }), commonjs(), babel({ extensions: ['.ts'], babelHelpers: 'runtime', include: ['src/**/*'] })]
+})
