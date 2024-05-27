00c29ea61b0802387571cadc699eb6de411a056c
丁言河D0362319 <dingyanhe@100.me>
2024-05-27 11:05:13 +0800

    chore: 处理相关镜像站

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,14 +1,40 @@
-import resolve from '@rollup/plugin-node-resolve'
-import babel from '@rollup/plugin-babel'
-import commonjs from '@rollup/plugin-commonjs'
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
 
-import { defineConfig } from 'rollup'
+import { defineConfig } from "rollup";
 
-export default defineConfig({
-	input: 'src/index.ts',
-	output: {
-		file: 'dist/bundle.js',
-		format: 'cjs'
-	},
-	plugins: [resolve({ extensions: ['.ts'] }), commonjs(), babel({ extensions: ['.ts'], babelHelpers: 'runtime', include: ['src/**/*'] })]
-})
+export default defineConfig([
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/bundle.js",
+      format: "cjs",
+    },
+    plugins: [
+      resolve({ extensions: [".ts"] }),
+      commonjs(),
+      babel({
+        extensions: [".ts"],
+        babelHelpers: "runtime",
+        include: ["src/**/*"],
+      }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/bundle.esm.js",
+      format: "esm",
+    },
+    plugins: [
+      resolve({ extensions: [".ts"] }),
+      commonjs(),
+      babel({
+        extensions: [".ts"],
+        babelHelpers: "runtime",
+        include: ["src/**/*"],
+      }),
+    ],
+  }
+]);
