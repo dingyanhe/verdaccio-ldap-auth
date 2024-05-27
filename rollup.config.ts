c5793ef4318fb776971f75e560a5b0cecfda26e9
丁言河D0362319 <dingyanhe@100.me>
2024-05-27 19:18:14 +0800

    chore: 简化构建

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
-
 import { defineConfig } from "rollup";
 
 export default defineConfig([
